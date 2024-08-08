9b6e1792c96d693f38d5f7b68b00201215669f49
farhanahmad95061 <farhanahmad95061@gmail.com>
2024-08-08 17:37:37 +0530

    Adding api

--- Frontend/src/components/Courses.jsx
+++ Frontend/src/components/Courses.jsx
@@ -11,7 +11,7 @@ function Courses() {
    useEffect(()=>{
     const getbook=async()=>{
       try {
-        const res= await axios.get("http://localhost:4001/book");
+        const res= await axios.get("https://farhans-library.onrender.com/book");
         console.log(res.data );
         setbook(res.data);
 
